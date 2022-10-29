05d7faae714dee868a61e8a0fc255028e5c3a17a
Владимир Дворный <vladimirdvornyj97017@gmail.com>
2022-10-29 09:26:50 +0500

    первая практика по Делегатам

---
+++ Делегаты/ClassLibrary1/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public delegate void AccountHandler(string message);
+    public class Class1 
+    {
+        public int Sum;
+        public string Fio;
+
+        public Class1(string fio, int sum)
+        {
+            Fio = fio;
+            Sum = sum;
+        }
+
+        int sum;
+        AccountHandler taken;
+        public Class1(int sum) => this.sum = sum;
+        // Регистрируем делегат
+        public void RegisterHandler(AccountHandler del)
+        {
+            taken += del;
+        }
+        // Отмена регистрации делегата
+        public void UnregisterHandler(AccountHandler del)
+        {
+            taken -= del; // удаляем делегат
+        }
+        public void Add(int sum) => this.sum += sum;
+        public void Take(int sum)
+        {
+            if (this.sum >= sum)
+            {
+                this.sum -= sum;
+                taken?.Invoke($"Со счета списано {sum} у.е.");
+            }
+            else
+                taken?.Invoke($"Недостаточно средств. Баланс: {this.sum} у.е.");
+        }
+    }
+}
---
+++ Делегаты/Делегаты/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary1;
+
+namespace Делегаты
+{
+    public partial class Form1 : Form
+    {
+        Class1 account;
+        AccountHandler Ach;
+        void PrintSimpleMessage(string message) => listBox1.Items.Add(message);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Bank
+        {
+            public int Sum;
+            public string Fio;
+
+            public Bank(int s, string f)
+            {
+                Sum = s;
+                Fio = f;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            account.Add(Convert.ToInt32(textBox1.Text));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 account = new Class1(200);
+            account.RegisterHandler(PrintSimpleMessage);
+            account.Take(100);
+            account.Take(150);
+
+            void PrintSimpleMessage(string message) => Console.WriteLine(message);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            account.Take(Convert.ToInt32(textBox1.Text));
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
